55c9355c172d7b855e73435c261e91e291f9bd70
Kevin Leung <kevinresol@gmail.com>
2021-07-27 12:28:30 +0800

    Update config

--- tests/RunTests.hx
+++ tests/RunTests.hx
@@ -5,12 +5,14 @@ import tink.unit.*;
 
 @:asserts
 class RunTests {
-
 	static function main() {
+		#if spawn_daemon
+		Sys.putEnv('DBUS_SESSION_BUS_ADDRESS', js.node.ChildProcess.execSync('dbus-daemon --fork --config-file=/usr/share/dbus-1/session.conf --print-address'));
+		#end
+		
 		Runner.run(TestBatch.make([
 			new SignatureTest(),
 			new ObjectTest(),
 		])).handle(Runner.exit);
 	}
-	
 }
