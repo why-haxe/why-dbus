12d082012195dc03d43c6e505358e9d7c8ca257a
Kevin Leung <kevinresol@gmail.com>
2021-07-28 14:27:36 +0800

    Cleanup a bit

--- src/why/dbus/Connection.hx
+++ src/why/dbus/Connection.hx
@@ -1,5 +1,11 @@
 package why.dbus;
 
+import why.dbus.server.Router;
+import why.dbus.server.Properties;
+import why.dbus.Message;
+
+using tink.CoreApi;
+
 class Connection {
 	final transport:Transport;
 	
@@ -9,4 +15,39 @@ class Connection {
 	
 	public macro function getInterface(ethis, destination, path, iface);
 	public macro function exportInterface(ethis, path, instance);
+	
+	function export<T>(path:String, iface:String, router:RouterBase<T>, properties:PropertiesBase<T>):CallbackLink {
+		return [
+			// listen for incoming calls
+			transport.calls.handle(pair -> {
+				final message = pair.a;
+				
+				function reply(outcome:Outcome<OutgoingReturnMessage, Error>) {
+					pair.b.invoke(switch outcome {
+						case Success(result):
+							Success(result);
+						case Failure(error):
+							Failure(({
+								name: error.message,
+								signature: cast '', // TODO: encode error data
+								body: [], // TODO: encode error data
+							}:OutgoingErrorMessage));
+					});
+				}
+				
+				if(message.path == path) {
+					if(message.iface == 'org.freedesktop.DBus.Properties') {
+						if(message.body[0] == iface) {
+							properties.route(message).handle(reply);
+						}
+					} else if(message.iface == iface) {
+						router.route(message).handle(reply);
+					}
+				}
+			}),
+			
+			// forward signals
+			router.signals.handle(transport.emit),
+		];
+	}
 }
--- src/why/dbus/Connection.macro.hx
+++ src/why/dbus/Connection.macro.hx
@@ -3,6 +3,7 @@ package why.dbus;
 import haxe.macro.Context;
 import haxe.macro.Expr;
 
+using tink.CoreApi;
 using tink.MacroApi;
 
 class Connection {
@@ -12,46 +13,14 @@ class Connection {
 		return macro (new why.dbus.client.Object<$ct>(@:privateAccess $ethis.transport, $destination, $path):why.dbus.client.Interface<$ct>);
 	}
 	
-	public macro function exportInterface(ethis:Expr, path:ExprOf<String>, instance:Expr):ExprOf<tink.core.Callback.CallbackLink> {
+	public macro function exportInterface(ethis:Expr, path:ExprOf<String>, instance:Expr):ExprOf<CallbackLink> {
 		return switch instance {
 			case macro ($value:$ct):
-				return macro {
-					final transport = @:privateAccess $ethis.transport;
+				macro {
 					final path = $path;
 					final iface = $v{ct.toString()};
 					final target = $value;
-					final router = new why.dbus.server.Router<$ct>(path, iface, target);
-					final properties = new why.dbus.server.Properties<$ct>(target);
-					
-					// listen for incoming calls
-					transport.calls.handle(pair -> {
-						final message = pair.a;
-						final reply = (outcome:tink.core.Outcome<why.dbus.Message.OutgoingReturnMessage, Error>) -> {
-							pair.b.invoke(switch outcome {
-								case Success(result):
-									Success(result);
-								case Failure(error):
-									Failure(({
-										name: error.message,
-										signature: cast '', // TODO: encode error data
-										body: [], // TODO: encode error data
-									}:why.dbus.Message.OutgoingErrorMessage));
-							});
-						}
-						
-						if(message.path == path) {
-							if(message.iface == 'org.freedesktop.DBus.Properties') {
-								if(message.body[0] == iface) {
-									properties.route(message).handle(reply);
-								}
-							} else if(message.iface == iface) {
-								router.route(message).handle(reply);
-							}
-						}
-					});
-					
-					// forward signals
-					router.signals.handle(transport.emit);
+					@:privateAccess $ethis.export(path, iface, new why.dbus.server.Router<$ct>(path, iface, target), new why.dbus.server.Properties<$ct>(target));
 				}
 			case _:
 				instance.pos.error('Expected check type syntax');
