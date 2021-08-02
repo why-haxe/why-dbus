f84f1971bf49748bb57eef4c06fe1b87c622ea01
Kevin Leung <kevinresol@gmail.com>
2021-08-02 15:03:58 +0800

    Some commonly used Property implementation

--- src/why/dbus/server/Property.hx
+++ src/why/dbus/server/Property.hx
@@ -5,6 +5,34 @@ using tink.CoreApi;
 typedef Getter<T> = () -> Promise<T>;
 typedef Setter<T> = T->Promise<Noise>;
 
+class SimpleProperty<T> extends Property<T> {
+	var value:T;
+	public function new(value:T) {
+		super(
+			() -> {
+				Promise.resolve(value);
+			},
+			v -> {
+				value = v;
+				Promise.NOISE;
+			}
+		);
+	}
+}
+class ClassicProperty<T> extends Property<T> {
+	public function new(get:()->T, set:T->Void) {
+		super(
+			() -> {
+				Promise.resolve(get());
+			},
+			v -> {
+				set(v);
+				Promise.NOISE;
+			}
+		);
+	}
+}
+
 class Property<T> implements ReadWriteProperty<T> {
 	final _get:Getter<T>;
 	final _set:Setter<T>;
