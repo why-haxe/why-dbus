200a7377b34f1a4d5519701eb044d230a8f2cc9f
Kevin Leung <kevinresol@gmail.com>
2021-08-31 10:58:51 +0800

    Make stuff lazy

--- src/why/dbus/client/Object.hx
+++ src/why/dbus/client/Object.hx
@@ -4,15 +4,22 @@ class Object {
 	public final destination:Destination;
 	public final path:String;
 	
-	public final properties:Interface<org.freedesktop.DBus.Properties>;
-	public final introspectable:Interface<org.freedesktop.DBus.Introspectable>;
+	public var properties(get, null):Interface<org.freedesktop.DBus.Properties>;
+	public var introspectable(get, null):Interface<org.freedesktop.DBus.Introspectable>;
 	
 	public function new(destination, path) {
 		this.destination = destination;
 		this.path = path;
-		
-		this.properties = getInterface(org.freedesktop.DBus.Properties);
-		this.introspectable = getInterface(org.freedesktop.DBus.Introspectable);
+	}
+	
+	function get_properties() {
+		if(properties == null) properties = getInterface(org.freedesktop.DBus.Properties);
+		return properties;
+	}
+	
+	function get_introspectable() {
+		if(introspectable == null) introspectable = getInterface(org.freedesktop.DBus.Introspectable);
+		return introspectable;
 	}
 	
 	public macro function getInterface(ethis, type);
